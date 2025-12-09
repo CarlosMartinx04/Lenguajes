274d6c7a7a9ee3f637de2093529b79f6c07e42c4
carlosMartinx04 <carloscokchi04@gmail.com>
2025-12-09 21:22:29 +0100

    dff

--- src/main/java/org/example/lenguajes/HelloApplication.java
+++ src/main/java/org/example/lenguajes/HelloApplication.java
@@ -1,18 +1,25 @@
 package org.example.lenguajes;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 400);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Empresa General DI");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/example/lenguajes/HelloController.java
+++ src/main/java/org/example/lenguajes/HelloController.java
@@ -1,14 +1,60 @@
 package org.example.lenguajes;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {
+    private ResourceBundle bundle;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label labelDetails = new Label();
+    @FXML
+    private ComboBox cbBoxLenguaje = new ComboBox<>();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setLanguage(Locale.getDefault());
+        SetLanguageNames();
+        cbBoxLenguaje.setValue(bundle.getString("cb.language.spanish"));
+        cbBoxLenguaje.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.equals(bundle.getString("cb.language.spanish"))) {
+                setLanguage(Locale.forLanguageTag("es_ES"));
+                cbBoxLenguaje.setValue(bundle.getString("cb.language.spanish"));
+            } else if (newValue.equals(bundle.getString("cb.language.english"))) {
+                setLanguage(Locale.forLanguageTag("en"));
+                cbBoxLenguaje.setValue(bundle.getString("cb.language.english"));
+            }
+            SetLanguageNames();
+        });
+
     }
+
+
+
+    private void SetLanguageNames() {
+        cbBoxLenguaje.getItems().clear();
+        cbBoxLenguaje.getItems().addAll(
+                bundle.getString("cb.language.spanish"),
+                bundle.getString("cb.language.english")
+        );
+    }
+    private void setLanguage(Locale locale) {
+        // Cambiar el ResourceBundle según el idioma seleccionado
+        bundle = ResourceBundle.getBundle("i18n/Messages", locale);
+        // Actualizar los textos de los Labels
+        labelDetails.setText(bundle.getString("lbl.labelDetails"));
+//        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG, locale);
+//        dateLabel.setText(dateFormat.format(now));
+//        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(locale);
+//        currencyLabel.setText(currencyFormat.format(currency));
+    }
+
+
 }
