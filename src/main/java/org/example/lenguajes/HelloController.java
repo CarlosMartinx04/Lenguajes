20315d9877937b0610880a604699fa97ff537fab
carlosMartinx04 <carloscokchi04@gmail.com>
2025-12-09 21:28:18 +0100

    SADASDA

--- src/main/java/org/example/lenguajes/HelloController.java
+++ src/main/java/org/example/lenguajes/HelloController.java
@@ -2,21 +2,34 @@ package org.example.lenguajes;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.TreeMap;
 
 public class HelloController implements Initializable {
     private ResourceBundle bundle;
     @FXML
     private Label labelDetails = new Label();
     @FXML
+    private Label labelInformationDetails = new Label();
+    @FXML
     private ComboBox cbBoxLenguaje = new ComboBox<>();
+    @FXML
+    private Button btnADDuser = new Button();
+    @FXML
+    private Button btnEDITuser = new Button();
+    @FXML
+    private Button btnDELETEuser = new Button();
+    @FXML
+    private Button btnUPDATEuser = new Button();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -50,6 +63,7 @@ public class HelloController implements Initializable {
         bundle = ResourceBundle.getBundle("i18n/Messages", locale);
         // Actualizar los textos de los Labels
         labelDetails.setText(bundle.getString("lbl.labelDetails"));
+        labelInformationDetails.setText(bundle.getString("lbl.labelInformationDetails"));
 //        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG, locale);
 //        dateLabel.setText(dateFormat.format(now));
 //        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(locale);
